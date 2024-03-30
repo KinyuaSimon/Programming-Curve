25de1ddbf476712b9dc7d2602a96d4c40a50b446
SimonK <65360400+KinyuaSimon@users.noreply.github.com>
2024-03-30 23:36:18 +0300

    Update C Language.c

--- C Language.c
+++ C Language.c
@@ -6,3 +6,11 @@ int main(){
 Return 0;
 
 }
+
+/* Understanding the #Header <Files> ** 
+
+** Header files include language declarations, in this case C language, and Macro declarations
+**System /language header files invoke system calls and libraries.
+the .h header files reference to the individual internal files either invoking calls to files related to the program.
+
+*/
